b1abb70bf2f5eab2128ad836593a1830b2064c68
Sam Grumbles <samgrumbles@yahoo.com>
2022-05-09 14:37:43 -0500

    model files added and updated

---
+++ models/Log.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Log extends Model {}
+
+Log.init(
+    {
+        id: {
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        round: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        hand_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'hand',
+                key: 'id',
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            reference: {
+                model: 'user',
+                key: 'user_id',
+            },
+        },
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        undersscored: true,
+        modelName: 'hand',
+    }
+);
+
+module.exports = Log;
