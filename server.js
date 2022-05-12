b647907f041e545668f867cbb669ed6a08120375
Alex Blanke <alexblanke98@gmail.com>
2022-05-12 14:36:54 -0500

    change back

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path');
+const express = require('express');
 const session = require('express-session');
-// const express = require('express');
 const {User} = require('./models');
 const helpers = require('./utils/helpers');
 
